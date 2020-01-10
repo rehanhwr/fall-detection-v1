7dc30daa35b73acbca66fb85cefefc34909b5eea
Rehan Hawari <rehan.hawari10@gmail.com>
2020-01-10 18:42:28 +0900

    update script, added num of epochs

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-#$ -cwd
-#$ -l q_node=1
-#$ -l h_rt=0:10:00
+##$ -cwd
+##$ -l q_node=1
+##$ -l h_rt=0:10:00
 . /etc/profile.d/modules.sh
 module load cuda/10.1.105 intel
-python ./squeeze_net2.py -d data
+python ./squeeze_net2.py -dn data -e 15
