0bd5bc9e68af1442310e567b60cebedb5d0f4f98
Rehan Hawari <rehan.hawari10@gmail.com>
2020-01-20 16:00:12 +0900

    reindent squeezenet

--- squeezenet.py
+++ squeezenet.py
@@ -101,81 +101,81 @@ def main(args):
 
 
 def train_model(model, dataloaders, criterion, optimizer, num_epochs):
-    since = time.time()
+  since = time.time()
 
-    val_acc_history = []
-    train_loss_history = []
+  val_acc_history = []
+  train_loss_history = []
 
-    best_model_wts = copy.deepcopy(model.state_dict())
-    best_acc = 0.0
+  best_model_wts = copy.deepcopy(model.state_dict())
+  best_acc = 0.0
 
-    for epoch in range(num_epochs):
-        since_epoch = time.time()
+  for epoch in range(num_epochs):
+    since_epoch = time.time()
 
-        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-        print('-' * 10)
+    print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+    print('-' * 10)
 
-        # Each epoch has a training and validation phase
-        for phase in ['train', 'val']:
-            if phase == 'train':
-                model.train()  # Set model to training mode
-            else:
-                model.eval()   # Set model to evaluate mode
+    # Each epoch has a training and validation phase
+    for phase in ['train', 'val']:
+      if phase == 'train':
+        model.train()  # Set model to training mode
+      else:
+        model.eval()   # Set model to evaluate mode
 
-            running_loss = 0.0
-            running_corrects = 0
+      running_loss = 0.0
+      running_corrects = 0
 
-            # Iterate over data.
-            for inputs, labels in dataloaders[phase]:
-                inputs = inputs.to(device)
-                labels = labels.to(device)
+      # Iterate over data.
+      for inputs, labels in dataloaders[phase]:
+        inputs = inputs.to(device)
+        labels = labels.to(device)
 
-                # zero the parameter gradients
-                optimizer.zero_grad()
+        # zero the parameter gradients
+        optimizer.zero_grad()
 
-                # forward
-                # track history if only in train
-                with torch.set_grad_enabled(phase == 'train'):
-                    # Get model outputs and calculate loss
-                    outputs = model(inputs)
-                    loss = criterion(outputs, labels)
+        # forward
+        # track history if only in train
+        with torch.set_grad_enabled(phase == 'train'):
+            # Get model outputs and calculate loss
+          outputs = model(inputs)
+          loss = criterion(outputs, labels)
 
-                    _, preds = torch.max(outputs, 1)
+          _, preds = torch.max(outputs, 1)
 
-                    # backward + optimize only if in training phase
-                    if phase == 'train':
-                        loss.backward()
-                        optimizer.step()
+          # backward + optimize only if in training phase
+          if phase == 'train':
+            loss.backward()
+            optimizer.step()
 
-                # statistics
-                running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(preds == labels.data)
+        # statistics
+        running_loss += loss.item() * inputs.size(0)
+        running_corrects += torch.sum(preds == labels.data)
 
-            epoch_loss = running_loss / len(dataloaders[phase].dataset)
-            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
+      epoch_loss = running_loss / len(dataloaders[phase].dataset)
+      epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
 
-            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+      print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
 
-            # deep copy the model
-            if phase == 'val' and epoch_acc > best_acc:
-                best_acc = epoch_acc
-                best_model_wts = copy.deepcopy(model.state_dict())
-            if phase == 'val':
-                val_acc_history.append(epoch_acc)
-            else:
-                train_loss_history.append(epoch_loss)
+      # deep copy the model
+      if phase == 'val' and epoch_acc > best_acc:
+          best_acc = epoch_acc
+          best_model_wts = copy.deepcopy(model.state_dict())
+      if phase == 'val':
+          val_acc_history.append(epoch_acc)
+      else:
+          train_loss_history.append(epoch_loss)
 
-        time_elapsed_epoch = time.time() - since_epoch
-        print('One epoch complete in {:.0f}m {:.0f}s'.format(time_elapsed_epoch // 60, time_elapsed_epoch % 60))
-        print()
+    time_elapsed_epoch = time.time() - since_epoch
+    print('One epoch complete in {:.0f}m {:.0f}s'.format(time_elapsed_epoch // 60, time_elapsed_epoch % 60))
+    print()
 
-    time_elapsed = time.time() - since
-    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
-    print('Best val Acc: {:4f}'.format(best_acc))
+  time_elapsed = time.time() - since
+  print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+  print('Best val Acc: {:4f}'.format(best_acc))
 
-    # load best model weights
-    model.load_state_dict(best_model_wts)
-    return model, train_loss_history, val_acc_history
+  # load best model weights
+  model.load_state_dict(best_model_wts)
+  return model, train_loss_history, val_acc_history
 
 if __name__ == '__main__':
   args = parse_args()
