1f783b1bd5d378930142165c7c8afbc3412b8500
Rehan Hawari <rehan.hawari10@gmail.com>
2020-01-27 02:58:05 +0900

    fix transformation

--- utils.py
+++ utils.py
@@ -100,14 +100,13 @@ def load_split_train_test(datadir, batch_size=64, input_size=224, valid_size = .
 
   data_transforms = {
     'train': transforms.Compose([
-      transforms.RandomResizedCrop(input_size),
+      transforms.Resize(input_size),
       transforms.RandomHorizontalFlip(),
       transforms.ToTensor(),
       transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
     'val': transforms.Compose([
       transforms.Resize(input_size),
-      transforms.CenterCrop(input_size),
       transforms.ToTensor(),
       transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
